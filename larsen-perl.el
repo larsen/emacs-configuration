1d20c5ee062f8af493b66e68da4d8f0885533c8d
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2014-05-25 15:28:15 +0200

    a couple of useful auto-modes + kbd bindinds

--- larsen-perl.el
+++ larsen-perl.el
@@ -1,3 +1,7 @@
+(global-set-key (kbd "C-h P") 'cperl-perldoc-at-point)
 
+(add-to-list 'auto-mode-alist '("\\.p[lm]$" . cperl-mode))
+(add-to-list 'auto-mode-alist '("\\.pod$" . pod-mode))
+(add-to-list 'auto-mode-alist '("\\.tt$" . tt-mode))
 
 (provide 'larsen-perl)
