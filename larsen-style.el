fd791107cd89b3ef1f98397dfd190a1b2a0be6b8
larsen <stefano.rodighiero@gmail.com>
2014-06-17 14:24:55 +0200

    color-theme, + anzu mode (for searches)

--- larsen-style.el
+++ larsen-style.el
@@ -1,7 +1,11 @@
 (set-default 'indent-tabs-mode nil)
 (setq-default tab-width 2)
 
+(require 'color-theme)
+    
+(color-theme-initialize)
 (color-theme-solarized-dark)
+
 (global-hl-line-mode 1) ; turn on highlighting current line
 (global-linum-mode 1)
 (global-visual-line-mode 1)
@@ -9,5 +13,8 @@
 (delete-selection-mode 1) ; delete seleted text when typing
 (show-paren-mode 1) ; turn on paren match highlighting
 (setq show-paren-style 'expression) ; highlight entire bracket expression
+
+(require 'anzu)
+(global-anzu-mode 1)
 
 (provide 'larsen-style)
