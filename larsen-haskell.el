46d0365f23e6242b1d95323737734e1156cd45f7
larsen <stefano.rodighiero@gmail.com>
2014-08-24 15:08:44 +0200

    re-enabled the mode; a few variations

--- larsen-haskell.el
+++ larsen-haskell.el
@@ -5,11 +5,17 @@
 (require 'haskell-mode)
 
 ; Too slow?
-; (autoload 'ghc-init "ghc" nil t)
-; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(autoload 'ghc-init "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 (setq auto-mode-alist (cons '("\.hs$" . haskell-mode) auto-mode-alist))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
 
 (provide 'larsen-haskell)
