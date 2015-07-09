f20758f09c8d9308b30e668c397dda42818d7a3f
larsen <stefano.rodighiero@gmail.com>
2015-07-09 08:51:22 +0200

    web mode for .html and .hbs files

--- larsen-web-mode.el
+++ larsen-web-mode.el
@@ -1,3 +1,4 @@
+(require 'web-mode)
 
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
@@ -6,5 +7,8 @@
 (setq web-mode-enable-control-block-indentation nil)
 (setq web-mode-style-padding 1)
 (setq web-mode-script-padding 1)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs?\\'" . web-mode))
 
 (provide 'larsen-web-mode)
