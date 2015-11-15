98277f46d225692d131ddc535fe8ffc6cfb8b08f
larsen <stefano.rodighiero@gmail.com>
2015-11-15 11:42:44 +0100

    WIP

--- larsen-haskell.el
+++ larsen-haskell.el
@@ -6,12 +6,13 @@
 
 ; Too slow?
 (autoload 'ghc-init "ghc" nil t)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
+(autoload 'ghc-init "/Applications/ghc-7.10.2.app/Contents/bin/ghc")
 
 (setq auto-mode-alist (cons '("\.hs$" . haskell-mode) auto-mode-alist))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 
 (custom-set-variables
   '(haskell-process-suggest-remove-import-lines t)
