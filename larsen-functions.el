a0ee81522b8e674efb3015b632ae6bc86fd26c4f
larsen <stefano.rodighiero@gmail.com>
2015-12-23 16:44:36 +0100

    a couple of functions stolen from magnars' .emacs

--- larsen-functions.el
+++ larsen-functions.el
@@ -38,4 +38,26 @@
 
 (global-set-key (kbd "M-<f1>") 'toggle-split-layout)
 
+
+;; from https://github.com/magnars/.emacs.d/blob/master/defuns/file-defuns.el
+
+(defun find-or-create-file-at-point-other-window ()
+  "Guesses what parts of the buffer under point is a file name and opens it."
+  (interactive)
+  (find-file-other-window (file-name-at-point)))
+
+(defun file-name-at-point ()
+  (save-excursion
+    (let* ((file-name-regexp "[./a-zA-Z0-9\-_~]")
+           (start (progn
+                    (while (looking-back file-name-regexp)
+                      (forward-char -1))
+                    (point)))
+           (end (progn
+                  (while (looking-at file-name-regexp)
+                    (forward-char 1))
+                  (point))))
+      (buffer-substring start end))))
+
 (provide 'larsen-functions)
+
