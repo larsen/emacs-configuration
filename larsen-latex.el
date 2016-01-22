7c8f61a88fb268314c0512afcb342d2e2fcea492
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-01-22 23:24:39 +0100

    AUCtex lastest changes

--- larsen-latex.el
+++ larsen-latex.el
@@ -1,29 +1,16 @@
-(setq TeX-auto-save t)
+(setq TeX-PDF-mode t)
+(setq Tex-auto-save t)
 (setq TeX-parse-self t)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-view-program-list '("zathura" "zathura %o"))
+(setq TeX-view-program-selection '((output-pdf "zathura")))
+(setq TeX-source-correlate-method 'synctex)
+(setq LaTeX-command "latex -synctex=1")
+
 (setq-default TeX-master nil)
 (add-hook 'LaTeX-mode-hook 'visual-line-mode)
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
-(setq TeX-PDF-mode t)
-
-(setq
- TeX-view-program-list '(("open" "open %o"))
-; TeX-view-program-selection '((output-pdf "open"))
-; TeX-source-correlate-method 'synctex
-; LaTeX-command "latex -synctex=1")
-)
-
-(getenv "PATH")
-(setenv "PATH"
-        (concat
-         "/usr/texbin" ":" (getenv "PATH")))
-
-(setenv "PATH"
-        (concat
-         "/usr/local/Cellar/ghostscript/9.14/bin/" ":" (getenv "PATH")))
-
-(setq exec-path (append exec-path '("/usr/local/Cellar/ghostscript/9.14/bin/")))
 
 (provide 'larsen-latex)
