e6cc2cf17e08589c9a391f71e6b92b22af0dce62
larsen <stefano.rodighiero@gmail.com>
2016-03-01 08:49:32 +0100

    more sensible setup (from http://blog.binchen.org/posts/how-to-be-extremely-efficient-in-emacs.html)

--- larsen-keyfreq.el
+++ larsen-keyfreq.el
@@ -1,4 +1,13 @@
 (require 'keyfreq)
+
+(setq keyfreq-excluded-commands
+      '(self-insert-command
+        abort-recursive-edit
+        forward-char
+        backward-char
+        previous-line
+        next-line))
+
 (keyfreq-mode 1)
 (keyfreq-autosave-mode 1)
 
