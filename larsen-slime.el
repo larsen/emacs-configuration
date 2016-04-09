92a2cdd038f964dbecd5189dc08f9ef937329f0a
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-04-09 11:14:38 +0200

    SLIME conf update

--- larsen-slime.el
+++ larsen-slime.el
@@ -1,16 +1,18 @@
-(add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-2.14/")
+; (add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-2.14/")
 
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-v2.17/")
+(setq inferior-lisp-program "/usr/bin/sbcl")
 
+; (require 'slime)
 (require 'slime-autoloads)
 (slime-setup '(slime-fancy))
 
-(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+; (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
 
-(global-set-key [(f2)] 'slime-hyperspec-lookup) 
+;; (load "~/quicklisp/clhs-use-local.el" t)
 
-; (load "~/quicklisp/clhs-use-local.el" t)
 (load "/home/larsen/quicklisp/clhs-use-local.el" t)
+(global-set-key [(f2)] 'slime-hyperspec-lookup) 
 
 (provide 'larsen-slime)
 
