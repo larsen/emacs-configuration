094cf8bb55113aaefadac77d6d3813858c3bd19a
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-07-17 21:16:12 +0200

    listing switches

--- larsen-dired.el
+++ larsen-dired.el
@@ -5,7 +5,7 @@
 (set-face-foreground 'diredp-file-name nil)
 (setq-default diredp-hide-details-initially-flag nil)
 
-; (setq dired-listing-switches "-lath")
+(setq dired-listing-switches "-lath")
 
 (setq dired-dwim-target t)
 
