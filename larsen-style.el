d0b4942a5db708fbfe5c7a83df5ea4b9e39b681d
larsen <stefano.rodighiero@gmail.com>
2016-07-20 18:48:37 +0200

    removed useless code

--- larsen-style.el
+++ larsen-style.el
@@ -2,15 +2,12 @@
 (setq-default tab-width 2)
 
 (global-hl-line-mode 1) ; turn on highlighting current line
-(global-linum-mode 1)
+(nlinum-mode)
 (global-visual-line-mode 1)
 (global-font-lock-mode 1)
 (delete-selection-mode 1) ; delete seleted text when typing
 (show-paren-mode 1) ; turn on paren match highlighting
 (setq show-paren-style 'expression) ; highlight entire bracket expression
-
-(require 'anzu)
-(global-anzu-mode 1)
 
 ; Not sure why I need this
 (setq ansi-term-color-vector
