3d2736a0219926ad901eb911676e334203ebebab
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-07-22 22:33:04 +0200

    WIP

--- larsen-refactoring.el
+++ larsen-refactoring.el
@@ -1,11 +1,15 @@
 ;; Functions to help Emacs Lisp refactoring
 
+(defun insert-new-function (function-name body)
+  (insert (pp `(defun ,function-name ,body))))
+
+(defun get-region ()
+  (buffer-substring-no-properties (mark) (point)))
+
 (defun create-new-function (function-name)
-  )
-
-(insert (pp '(foo (bar))))
-
-
+  (interactive "sFunction name: ")
+  (insert-new-function function-name
+                       (eval (get-region))))
 
 (provide 'larsen-refactoring)
 
