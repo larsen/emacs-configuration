8cd08342c4584c05471b09e0a175391cc1ca943e
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-05-01 12:49:13 +0200

    differentiates clj and cljs support

--- larsen-clojure.el
+++ larsen-clojure.el
@@ -1,6 +1,7 @@
 (require 'clojure-mode)
 
-(add-to-list 'auto-mode-alist '("\\.cljs?$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs$" . clojurescript-mode))
 (add-to-list 'auto-mode-alist '("\\.cljs?$" . paredit-mode))
 
 (provide 'larsen-clojure)
