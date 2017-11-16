7f86051acf55e6b0e80094f5085a535674148a9b
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-11-16 18:26:47 +0100

    work in progress

--- larsen-clojure.el
+++ larsen-clojure.el
@@ -1,4 +1,5 @@
 (require 'clojure-mode)
+(require 'clj-refactor)
 
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.cljs$" . clojurescript-mode))
@@ -6,5 +7,13 @@
 
 (setq cider-repl-use-pretty-printing t)
 (setq cider-prompt-for-symbol nil)
+
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 (provide 'larsen-clojure)
