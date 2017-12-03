69de94e5836ca8d3c11b44dd381bf199b63e41cb
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2017-12-03 19:18:13 +0100

    upgraded slime workaround for misbehaviour of autodoc with eldoc (implicit require via slime-fancy, AFAIU)

--- larsen-slime.el
+++ larsen-slime.el
@@ -1,16 +1,33 @@
 ; (add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-2.14/")
 
-(add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-v2.17/")
+(add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/slime-v2.20/")
 (setq inferior-lisp-program "/usr/bin/sbcl")
 
 ; (require 'slime)
 (require 'slime-autoloads)
-(slime-setup '(slime-fancy
+(slime-setup '(;; slime-fancy
+               slime-repl
+               slime-c-p-c
+               slime-editing-commands
+               slime-fancy-inspector
+               slime-fancy-trace
+               slime-scratch
+               slime-trace-dialog
+               slime-presentations
+               
                slime-asdf
                slime-tramp
                slime-indentation))
 
 ; (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+
+
+(setf slime-mode-hook
+      '(slime-trace-dialog-enable
+       ; slime-autodoc--on
+       slime-add-easy-menu
+       slime-setup-first-change-hook))
+
 (add-hook 'lisp-mode-hook #'paredit-mode)
 
 ;; (load "~/quicklisp/clhs-use-local.el" t)
