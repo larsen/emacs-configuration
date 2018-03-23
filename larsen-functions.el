2cdf18228dbf9c7325ae66bd8295ff1f0407dcd4
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2018-03-23 11:29:37 +0100

    + unfill_paragraph

--- larsen-functions.el
+++ larsen-functions.el
@@ -67,6 +67,15 @@
       (buffer-substring start end))))
 
 
+;; https://www.emacswiki.org/emacs/UnfillParagraph
+
+(defun unfill-paragraph ()
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+
 ;; utility to use with shr (eww, elfeed entry view, ...)
 
 (require 'f)
