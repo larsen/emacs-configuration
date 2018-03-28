bcea1bce8152716c4b6be0d288e5bd41c5494e78
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2018-03-28 10:40:28 +0200

    gets PG connections details from ~/.pgpass

--- larsen-sql.el
+++ larsen-sql.el
@@ -3,6 +3,23 @@
 (eval-after-load "sql"
   (load-library "sql-indent"))
 
+(require 'cl)
+(require 'cl-loop)
+
+;; Gets PG connections details from ~/.pgpass file
+(setq sql-connection-alist
+      (with-current-buffer (find-file-noselect "~/.pgpass")
+        (let ((lines (split-string (buffer-string) "\n" t)))
+          (when lines
+            (loop for l in lines
+                  collect (destructuring-bind
+                              (host port db user password) (split-string l ":" nil)
+                            `(,db (sql-product 'postgres)
+                                  (sql-port ,(string-to-number port))
+                                  (sql-server ,host)
+                                  (sql-user ,user)
+                                  (sql-database ,db))))))))
+
 (setq sql-mysql-program "/usr/local/mysql/bin/mysql")
 (defun my-sql-save-history-hook ()
   (let ((lval 'sql-input-ring-file-name)
