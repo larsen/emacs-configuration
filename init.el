ca8971145b22b1617456699e324c9452d5d642cd
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2018-06-07 11:44:00 +0200

    fixed DOCSTRING

--- init.el
+++ init.el
@@ -18,7 +18,7 @@
 (require 'package)
 (package-initialize)
 (defun package-require (pkg)
-  "Install a package only if it's not already installed."
+  "Install a package PKG only if it's not already installed."
   (when (not (package-installed-p pkg))
     (package-install pkg)))
 
