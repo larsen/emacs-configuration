73ae3075d28e58c21c1c55a9df94157fac4814d5
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2018-06-18 15:20:35 +0200

    sort dired listings with directories first

--- larsen-dired.el
+++ larsen-dired.el
@@ -4,4 +4,18 @@
 
 (setq dired-dwim-target t)
 
+;; Copied from https://github.com/IvanRave/emacs-config/blob/master/init.el
+(defun mydired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+    (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  (mydired-sort))
+
 (provide 'larsen-dired)
