f6ed75f54b8acacaf2eb6658004e0fcca1322e96
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-03-30 16:25:44 +0100

    eval-and-replace

--- larsen-functions.el
+++ larsen-functions.el
@@ -180,4 +180,16 @@
      (kill-this-buffer)
      (delete-frame))))
 
+;; https://emacsredux.com/blog/2013/06/21/eval-and-replace/
+
+(defun eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (condition-case nil
+      (prin1 (eval (read (current-kill 0)))
+             (current-buffer))
+    (error (message "Invalid expression")
+           (insert (current-kill 0)))))
+
 (provide 'larsen-functions)
--- larsen-keys.el
+++ larsen-keys.el
@@ -19,6 +19,7 @@
        (,(kbd "C-c t") (lambda () (interactive) (ansi-term "/bin/bash")))
        (,(kbd "C-c m c") mc/edit-lines)
        (,(kbd "C-c e m") macrostep-mode)
+       (,(kbd "C-c e r") eval-and-replace)
        (,(kbd "C-c m a") mc/mark-all-like-this)
        (,(kbd "C-S-<mouse-1>") mc/add-cursor-on-click)))
 
