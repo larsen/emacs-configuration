a9146b46e425baa56eb9111cf83570b8824df426
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-05-08 20:47:53 +0200

    fixed indentation

--- larsen-flycheck.el
+++ larsen-flycheck.el
@@ -6,10 +6,10 @@
 
 (add-to-list 'display-buffer-alist
              `(,(rx bos "*Flycheck errors*" eos)
-              (display-buffer-reuse-window
-               display-buffer-in-side-window)
-              (side            . bottom)
-              (reusable-frames . visible)
-              (window-height   . 0.33)))
+               (display-buffer-reuse-window
+                display-buffer-in-side-window)
+               (side            . bottom)
+               (reusable-frames . visible)
+               (window-height   . 0.33)))
 
 (provide 'larsen-flycheck)
