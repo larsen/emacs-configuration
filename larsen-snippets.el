f2fe3373ea645282f8437c72d60c876db3a4795c
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2019-07-21 16:06:00 +0200

    removed more stale code

--- larsen-snippets.el
+++ larsen-snippets.el
@@ -3,8 +3,4 @@
 (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
 
-;(define-key yas-minor-mode-map (kbd "<tab>") nil)
-;(define-key yas-minor-mode-map (kbd "TAB") nil)
-;(define-key yas-minor-mode-map (kbd "[?\C-\t]") 'yas-expand)
-
 (provide 'larsen-snippets)
