1d0928374181679c8c1dc638984c6148de5386e0
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-02-23 11:49:48 +0100

    a bit of refactoring, and making elpy-modules settings explicit

--- larsen-python.el
+++ larsen-python.el
@@ -1,22 +1,29 @@
 ;;; larsen-python
-;;; Loads elpy
 
 (require 'elpy)
+(require 'pyvenv)
+(require 'blacken)
 
 (elpy-enable)
 
-(setq elpy-rpc-backend "jedi")
+(setq elpy-rpc-backend "jedi"
+      elpy-use-ipython t
+      elpy-rpc-virtualenv-path "~/work/idagio/.venv37/"  ; or 'current?
+      elpy-modules '(elpy-module-sane-defaults
+                     elpy-module-company
+                     elpy-module-eldoc
+                     elpy-module-highlight-indentation
+                     elpy-module-pyvenv
+                     elpy-module-yasnippet
+                     elpy-module-django))
 
 (setq-default py-indent-offset 2)
 
 (add-hook 'python-mode-hook 'blacken-mode)
 (add-hook 'python-mode-hook  (lambda () (flycheck-mode -1)))
 
-;; (setq elpy-rpc-virtualenv-path 'current)
-(setq elpy-rpc-virtualenv-path "~/work/idagio/.venv37/")
-
 (when (load "flycheck" t t)
-  ; (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (provide 'larsen-python)
