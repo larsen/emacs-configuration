f339ed6642e29b8babc3a89afc4da6db69c92625
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-03-14 14:44:39 +0100

    typo

--- larsen-anki.el
+++ larsen-anki.el
@@ -1,9 +1,9 @@
 (setq org-my-anki-file "~/org/personal/anki.org")
 
 (add-to-list 'org-capture-templates
-             ("a" "Anki basic"
-              entry
-              (file+headline org-my-anki-file "Dispatch Shelf")
-              "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Basic\n:ANKI_DECK: Default\n:END:\n** Front\n%?\n** Back\n%x\n"))
+             `("a" "Anki basic"
+               entry
+               (file+headline org-my-anki-file "Dispatch Shelf")
+               "* %<%H:%M>   %^g\n:PROPERTIES:\n:ANKI_NOTE_TYPE: Basic\n:ANKI_DECK: Default\n:END:\n** Front\n%?\n** Back\n%x\n"))
 
 (provide 'larsen-anki)
