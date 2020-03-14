d3e98fa1671c7576b4bb623a880db3061698a0b7
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-03-14 14:57:52 +0100

    a bit of cleaning

--- larsen-erc.el
+++ larsen-erc.el
@@ -1,25 +1,18 @@
-(setq erc-hide-list '("JOIN" "PART" "QUIT"))
+(load "~/.ercpass")
 
-(add-hook 'erc-text-matched-hook 'my-erc-notify-hook)
-
-;; (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#erc")))
-
-;; Make C-c RET (or C-c C-RET) send messages instead of RET. This has
-;; been commented out to avoid confusing new users.
-;; (define-key erc-mode-map (kbd "RET") nil)
-;; (define-key erc-mode-map (kbd "C-c RET") 'erc-send-current-line)
-;; (define-key erc-mode-map (kbd "C-c C-RET") 'erc-send-current-line)
+(require 'erc-ring)
+(erc-ring-mode)
 
 (require 'erc-image)
 ;; (add-to-list 'erc-modules 'image)
 ;; (setq erc-image-inline-rescale 300)
 (erc-update-modules)
 
-(setq erc-fill-function 'erc-fill-static)
-(setq erc-fill-static-center 16)
+(add-hook 'erc-text-matched-hook 'my-erc-notify-hook)
 
-(setq erc-prompt (lambda () (concat "[" (buffer-name) "]")))
-
-(load "~/.ercpass")
+(setq erc-hide-list '("JOIN" "PART" "QUIT")
+      erc-fill-function 'erc-fill-static
+      erc-fill-static-center 16
+      erc-prompt (lambda () (concat "[" (buffer-name) "]")))
 
 (provide 'larsen-erc)
