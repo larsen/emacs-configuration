c1002d73ef916d7772ae0ebcbe25ecbd259046bb
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-06-27 10:53:13 +0200

    Helm addons (projectile and ag)

--- larsen-helm.el
+++ larsen-helm.el
@@ -1,6 +1,8 @@
 (add-to-list 'load-path (concat dotfiles-dir "site-lisp/helm"))
 
 (use-package helm-config)
+(use-package helm-projectile)
+(use-package helm-ag)
 
 (custom-set-variables '(helm-ff-lynx-style-map t))
 (helm-mode 1)
