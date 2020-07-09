78fd210ee1736181d3ece32180616cc37907544b
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-07-09 09:53:48 +0200

    additional categories

--- larsen-ibuffer.el
+++ larsen-ibuffer.el
@@ -12,8 +12,13 @@
                      (name . "slime")))
          ("Emacs Lisp" (or (mode . emacs-lisp-mode)
                            (name . "^\\*ielm\\*$")))
+         ("ERC" (mode . erc-mode))
+         ("SQL" (mode . sql-mode))
+         ("Helm" (mode . helm-major-mode))
+         ("PDF" (mode . pdf-view-mode))
          ("System" (or (name . "^\\*scratch\\*$")
-                       (name . "^\\*Messages\\*$"))))))
+                       (name . "^\\*Messages\\*$")
+                       (name . "^\\*Process list\\*$"))))))
 
 (setq ibuffer-formats
       '((mark modified read-only " "
