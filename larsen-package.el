da77f6681bb7aeb8a42443577d177472c20203d0
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-08-27 17:42:18 +0200

    changed address for Melpa

--- larsen-package.el
+++ larsen-package.el
@@ -4,6 +4,6 @@
 
 (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa"     . "http://melpa.milkbox.net/packages/")))
+                         ("melpa"     . "https://melpa.org/packages/")))
 
 (provide 'larsen-package)
