8fb9fe84171920f9e82b26ab853734732f85c3e5
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-10-23 15:18:03 +0200

    additional chord

--- larsen-keys.el
+++ larsen-keys.el
@@ -87,7 +87,9 @@
 (my-global-set-keys (larsen-keys))
 
 (key-chord-mode t)
+(setq key-chord-two-keys-delay 0.05)
 (key-chord-define-global ",," 'slime-selector)
+(key-chord-define-global "//" 'isearch-forward)
 (key-chord-define-global "hh" 'helm-mini)
 (key-chord-define-global "jj" 'ace-jump-mode)
 
