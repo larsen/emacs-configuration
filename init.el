37f34acbada3580c46f09a5d1ed06d94fa2c829c
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-11-10 18:11:57 +0100

    not sure why I need this :shrug:

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8-unix)
 
 ;; Auto refresh buffers
 (global-auto-revert-mode 1)
