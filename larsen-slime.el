344fec5eb9dbfd74d9693e79f2deb318b2469856
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2020-12-13 17:19:51 +0100

    messed up my SBCL installation, need to point to a working one more precisely

--- larsen-slime.el
+++ larsen-slime.el
@@ -1,5 +1,5 @@
 (load (expand-file-name "~/.quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "/usr/bin/sbcl")
 
 ; (use-package 'slime)
 (use-package slime-autoloads)
