4aa942fb915ebac9cf7d4c9c0dadbc6f953cb745
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-01-03 17:17:32 +0100

    whitespace mode enabled for Lisp-y languages

--- larsen-slime.el
+++ larsen-slime.el
@@ -27,6 +27,7 @@
        slime-setup-first-change-hook))
 
 (add-hook 'lisp-mode-hook #'paredit-mode)
+(add-hook 'lisp-mode-hook #'whitespace-mode)
 (add-hook 'slime-repl-mode-hook
           (lambda () (paredit-mode t)))
 
