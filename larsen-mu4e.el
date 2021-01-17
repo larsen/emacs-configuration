6b079a3a87231359acb0ec071b6d6409bd58128f
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-01-17 11:23:08 +0100

    custom bookmarks

--- larsen-mu4e.el
+++ larsen-mu4e.el
@@ -15,18 +15,27 @@
       mu4e-headers-visible-columns 100
       user-mail-address "stefano.rodighiero@gmail.com"
 
-      mu4e-show-images t)
+      mu4e-show-images t
+      mu4e-bookmarks '((:name "Unread messages"
+                              :query "flag:unread AND NOT flag:trashed"
+                              :key ?u)
+                       (:name "Unread messages (no ML)"
+                              :query "flag:unread AND NOT flag:trashed AND NOT flag:list"
+                              :key ?m)
+                       (:name "Today's messages"
+                              :query "date:today..now"
+                              :key ?t)
+                       (:name "Last 7 days"
+                              :query "date:7d..now"
+                              :hide-unread t
+                              :key ?w)
+                       (:name "Messages with images"
+                              :query "mime:image/*"
+                              :key ?p)))
 
 ;; use imagemagick, if available
 (when (fboundp 'imagemagick-register-types)
   (imagemagick-register-types))
-
-;; (setq mu4e-bookmarks
-;;   '(("flag:unread AND NOT flag:trashed" "Unread messages"      ?i)
-;;     ("date:today..now"                  "Today's messages"     ?t)
-;;     ("date:today..now AND flag:unread AND NOT flag:trashed" "Today's Unread messages"     ?u)
-;;     ("date:7d..now"                     "Last 7 days"          ?w)
-;;     ("mime:image/*"                     "Messages with images" ?p)))
 
 ;; (use-package mu4e-alert
 ;;   :ensure t
