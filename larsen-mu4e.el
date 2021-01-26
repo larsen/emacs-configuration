36f969d5479603a6ea5629f604f61b169ada5d2c
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-01-26 18:14:17 +0100

    mu4e-alert configuration

--- larsen-mu4e.el
+++ larsen-mu4e.el
@@ -37,18 +37,10 @@
 (when (fboundp 'imagemagick-register-types)
   (imagemagick-register-types))
 
-;; (use-package mu4e-alert
-;;   :ensure t
-;;   :after mu4e
-;;   :init
-;;   (setq mu4e-alert-interesting-mail-query
-;;     (concat "flag:unread maildir:/[Gmail]/Inbox"))
-;;   (mu4e-alert-enable-mode-line-display)
-;;   (defun gjstein-refresh-mu4e-alert-mode-line ()
-;;     (interactive)
-;;     (mu4e~proc-kill)
-;;     (mu4e-alert-enable-mode-line-display))
-;;   (run-with-timer 0 60 'gjstein-refresh-mu4e-alert-mode-line))
+(use-package mu4e-alert :ensure t)
+(mu4e-alert-set-default-style 'libnotify)
+(add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+(add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
 
 (require 'smtpmail)
 (setq message-send-mail-function 'smtpmail-send-it
