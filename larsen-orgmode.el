252619c93ba2ae8c6e0192b9bc784bcca4f0d8f8
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-02-16 17:25:31 +0100

    enabled Lisp for org-babel

--- larsen-orgmode.el
+++ larsen-orgmode.el
@@ -63,7 +63,8 @@ WEEK-DAY is expressed as an integer in the range 0..6:
         (gnuplot . t)
         (R . t)
         (plantuml . t)
-        (http . t)))
+        (http . t)
+        (lisp . t)))
 
 (defun my-insert-current-week-item (week-number)
   "Insert 1-week-worth structure for entering todo entries for WEEK-NUMBER."
