e99b1760c7efaf6f45c7e273b718f040111dcc85
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2021-11-03 14:30:13 +0100

    removed package archive

--- larsen-package.el
+++ larsen-package.el
@@ -3,7 +3,6 @@
 ;;;
 
 (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa"     . "https://melpa.org/packages/")))
 
 (provide 'larsen-package)
