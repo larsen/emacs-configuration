e6d2741c8252b0af951aafa97a17383deec231be
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-08-23 17:07:24 +0200

    refactoring

--- lisp/larsen-pdf-tools.el
+++ lisp/larsen-pdf-tools.el
@@ -1,9 +1,25 @@
-(pdf-tools-install t)
-
-;; I need to remove pdf-cache-prefetch-minor-mode from
-;; pdf-tools-enabled-modes, to open PDF from the file manager (not
-;; sure why it does not work otherwise, but it seems confirmed
-;; experimentally.
+(use-package pdf-tools
+  :config
+  (pdf-tools-install :no-query)
+  :custom
+  (pdf-tools-enabled-modes '(pdf-history-minor-mode
+                             pdf-isearch-minor-mode
+                             pdf-links-minor-mode
+                             pdf-misc-minor-mode
+                             pdf-outline-minor-mode
+                             pdf-misc-size-indication-minor-mode
+                             pdf-misc-menu-bar-minor-mode
+                             pdf-annot-minor-mode
+                             pdf-sync-minor-mode
+                             pdf-misc-context-menu-minor-mode
+                             ;; I need to remove pdf-cache-prefetch-minor-mode from
+                             ;; pdf-tools-enabled-modes, to open PDF from the file manager (not
+                             ;; sure why it does not work otherwise, but it seems confirmed
+                             ;; experimentally.
+                             ;; pdf-cache-prefetch-minor-mode
+                             pdf-occur-global-minor-mode
+                             ;; pdf-virtual-global-minor-mode
+                             )))
 
 ;; Other parts of the configuration in:
 ;; /home/stefano/.local/share/applications/emacsclient.desktop
@@ -12,24 +28,9 @@
 ;;   emacsclient.desktop
 
 ;; ansi-term
-
 ;; See https://emacs.stackexchange.com/questions/39312/output-reflow-in-ansi-term
 
-(setq pdf-tools-enabled-modes
-      '(pdf-history-minor-mode
-       pdf-isearch-minor-mode
-       pdf-links-minor-mode
-       pdf-misc-minor-mode
-       pdf-outline-minor-mode
-       pdf-misc-size-indication-minor-mode
-       pdf-misc-menu-bar-minor-mode
-       pdf-annot-minor-mode
-       pdf-sync-minor-mode
-       pdf-misc-context-menu-minor-mode
-       ;; pdf-cache-prefetch-minor-mode
-       pdf-occur-global-minor-mode
-       ;; pdf-virtual-global-minor-mode
-       ))
+
 (setq-default pdf-view-display-size 'fit-page)
 
 ;; Taken from http://pragmaticemacs.com/emacs/even-more-pdf-tools-tweaks/
