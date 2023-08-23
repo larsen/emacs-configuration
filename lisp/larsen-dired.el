55c364879dc7afada826471bc58f2639cc3e8801
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-08-23 13:23:18 +0200

    refactoring

--- lisp/larsen-dired.el
+++ lisp/larsen-dired.el
@@ -1,30 +1,30 @@
-; (package-require 'dired+)
+(use-package dired
+  :custom
+  (dired-listing-switches "-aGFhlS --group-directories-first --time-style=long-iso")
+  (dired-dwim-target t)
+  (dired-recursive-deletes 'always)
+  (dired-recursive-copies 'always)
+  (dired-deletion-confirmer 'y-or-n-p)
+  (dired-clean-confirm-killing-deleted-buffers nil)
+  (dired-clean-up-buffers-too nil)
+  (dired-guess-shell-alist-user '(("\\.pdf\\'" "evince")
+                                  ("\\.jpg\\'" "feh")
+                                  ("\\.gpx\\'" "viking")
+                                  ("\\.kml\\'" "viking"))))
+
+(use-package peep-dired
+  :custom
+  (peep-dired-cleanup-on-disable t)
+  (peep-dired-cleanup-eagerly nil)
+  (peep-dired-ignored-extensions '("mkv" "iso" "mp4" "mp3" "zip" "tgz" "gz" "xz" "flac")))
 
 (use-package dired-x)
 (use-package dired-avfs)
-(use-package peep-dired)
-(use-package diff-hl)
 
+(use-package diff-hl)
 
 ;; Useful combo
 ;; Ctrl-u s -- interactively change the 'dired-listing-switches
-
-(setq dired-listing-switches "-aGFhlS --group-directories-first --time-style=long-iso"
-      dired-dwim-target t
-      dired-recursive-deletes 'always
-      dired-recursive-copies 'always
-      dired-deletion-confirmer 'y-or-n-p
-      dired-clean-confirm-killing-deleted-buffers nil
-      dired-clean-up-buffers-too nil
-      dired-guess-shell-alist-user
-      	  '(("\\.pdf\\'" "evince")
-            ("\\.jpg\\'" "feh")
-            ("\\.gpx\\'" "viking")
-            ("\\.kml\\'" "viking")))
-
-(setq peep-dired-cleanup-on-disable t
-      peep-dired-cleanup-eagerly nil
-      peep-dired-ignored-extensions '("mkv" "iso" "mp4" "mp3" "zip" "tgz" "gz" "xz" "flac"))
 
 (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
 
