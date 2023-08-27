f826c7116bfdc8126ce7a6387cd329cc603771ae
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-08-27 09:13:33 +0200

    bindings in the right place

--- lisp/larsen-helm.el
+++ lisp/larsen-helm.el
@@ -6,15 +6,16 @@
          ("C-M-y" . helm-mini)
          ("<print>" . helm-mini)
          ("M-x" . helm-M-x)
-         ("C-c s" . helm-swoop)
-         ("C-c C-s" . helm-swoop-projectile)
          ("C-c v" . helm-show-kill-ring)
          ("C-x C-f" . helm-find-files))
   :custom
   (helm-ff-lynx-style-map t)
   (helm-buffer-max-length 60))
 
-(use-package helm-swoop :requires helm)
+(use-package helm-swoop :requires helm
+  :bind (("C-c s" . helm-swoop)
+         ("C-c C-s" . helm-swoop-projectile)))
+
 (use-package helm-config :requires helm)
 (use-package helm-projectile
   :requires helm
