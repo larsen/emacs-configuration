2165129bed2fa1665e873f1625eb298604b659e3
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-09-01 16:16:26 +0200

    refactoring

--- lisp/larsen-erc.el
+++ lisp/larsen-erc.el
@@ -1,14 +1,26 @@
 (load "~/.ercpass")
 
-(use-package erc-image)
-(erc-update-modules)
+(use-package erc-image
+  :config
+  (erc-update-modules)
+  (defalias 'notify
+    (if (eq system-type 'darwin)
+        (apply-partially 'osx-notify)
+      (apply-partially 'linux-notify)))
+  (defun my-erc-notify-hook (match-type nick message)
+    (when (eq match-type 'current-nick)
+      (unless (posix-string-match "^\\** *Users on #" message)
+        (notify (concat "ERC " (buffer-name (current-buffer)))
+                message))))
+  (add-hook 'erc-text-matched-hook 'my-erc-notify-hook)
+  :custom
+  (erc-hide-list '("JOIN" "PART" "QUIT"))
+  (erc-fill-function 'erc-fill-static)
+  (erc-fill-static-center 16)
+  (erc-prompt (lambda () (concat "[" (buffer-name) "]"))))
 
-(add-hook 'erc-text-matched-hook 'my-erc-notify-hook)
 
-(setq erc-hide-list '("JOIN" "PART" "QUIT")
-      erc-fill-function 'erc-fill-static
-      erc-fill-static-center 16
-      erc-prompt (lambda () (concat "[" (buffer-name) "]")))
+
 
 
 ; irc.libera.chat
