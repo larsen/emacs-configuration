29d9e38eca73118f95d08f84e9b3b2ba26e5892a
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-09-08 16:04:24 +0200

    typo

--- lisp/larsen-env.el
+++ lisp/larsen-env.el
@@ -53,7 +53,7 @@
   :bind (("C-c m c" . mc/edit-lines)
          ("C-c m a" . mc/mark-all-like-this)
          ("C-c m n" . mc/mark-next-like-this)
-         ("C-S-<mouse-1>" . mc/add-cursor-on-clicko)))
+         ("C-S-<mouse-1>" . mc/add-cursor-on-click)))
 
 (use-package editorconfig
   :config (editorconfig-mode 1))
