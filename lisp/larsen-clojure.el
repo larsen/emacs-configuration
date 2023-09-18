bb75e356568eeb986558b55044d4624c2e224b35
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-09-18 17:15:18 +0200

    refactoring Clojure conf (WIP)

--- lisp/larsen-clojure.el
+++ lisp/larsen-clojure.el
@@ -1,24 +1,28 @@
-(use-package clojure-mode)
+(use-package clojure-mode
+  :mode ("\\.clj$" . clojure-mode)
+  :hook (clojure-mode . paredit-mode)
+  :custom
+  (cider-repl-use-pretty-printing t)
+  (cider-prompt-for-symbol nil))
+
+(use-package clojurescript-mode
+  :mode ("\\.cljs$" . clojurescript-mode))
+
 (use-package clj-refactor)
 
 ; (add-to-list 'auto-mode-alist '("\\.cljs?$" . paredit-mode))
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.cljs$" . clojurescript-mode))
 
-(setq cider-repl-use-pretty-printing t)
-(setq cider-prompt-for-symbol nil)
+;; (defun my-clojure-mode-hook ()
+;;     (clj-refactor-mode 1)
+;;     (yas-minor-mode 1) ; for adding require/use/import statements
+;;     ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+;;     (cljr-add-keybindings-with-prefix "C-c C-m"))
 
-(defun my-clojure-mode-hook ()
-    (clj-refactor-mode 1)
-    (yas-minor-mode 1) ; for adding require/use/import statements
-    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-    (cljr-add-keybindings-with-prefix "C-c C-m"))
+;; (add-hook 'cider-stacktrace-mode-hook
+;;           (lambda ()
+;;             (set (make-local-variable 'comment-start) ";;")
+;;             (set (make-local-variable 'comment-end) "\n")))
 
-(add-hook 'cider-stacktrace-mode-hook
-          (lambda ()
-            (set (make-local-variable 'comment-start) ";;")
-            (set (make-local-variable 'comment-end) "\n")))
-
-(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+;; (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 (provide 'larsen-clojure)
