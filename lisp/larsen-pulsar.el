f607980a2deb14a88d87ca9cecf04a934f7eb019
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2023-10-07 14:19:08 +0200

    changed pulsar color

--- lisp/larsen-pulsar.el
+++ lisp/larsen-pulsar.el
@@ -4,7 +4,7 @@
   (pulsar-pulse t)
   (pulsar-delay 0.055)
   (pulsar-iterations 10)
-  (pulsar-face 'pulsar-cyan)
+  (pulsar-face 'pulsar-yellow)
   (pulsar-highlight-face 'pulsar-yellow)
   :config
   (pulsar-global-mode 1))
