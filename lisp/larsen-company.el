3d39049c7ebfca7c15363450c343699b588cb08b
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-03-06 08:01:04 +0100

    company: Python support should be revisited

--- lisp/larsen-company.el
+++ lisp/larsen-company.el
@@ -2,9 +2,4 @@
   :config
   (global-company-mode))
 
-(defun my/python-mode-hook ()
-  (add-to-list 'company-backends 'company-jedi))
-
-(add-hook 'python-mode-hook 'my/python-mode-hook)
-
 (provide 'larsen-company)
