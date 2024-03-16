48a2a73014ed4f306795a5c1cd386f3873799c5a
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-03-16 15:21:01 +0100

    a couple of tweaks

--- lisp/larsen-company.el
+++ lisp/larsen-company.el
@@ -1,5 +1,10 @@
 (use-package company
   :config
-  (global-company-mode))
+  (global-company-mode)
+  :custom
+  (company-selection-wrap-around t)
+  (company-idle-delay (lambda () (if (company-in-string-or-comment) nil 0.3)))
+  ; (company-global-modes '(not erc-mode message-mode eshell-mode))
+  )
 
 (provide 'larsen-company)
