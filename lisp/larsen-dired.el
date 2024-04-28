904c6e9d8c5bf0ac7cd4dc1122f2fbec099ac7cb
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-04-28 10:05:27 +0200

    different params for directory listing; all-the-icons

--- lisp/larsen-dired.el
+++ lisp/larsen-dired.el
@@ -1,6 +1,6 @@
 (use-package dired
   :custom
-  (dired-listing-switches "-aGFhlS --group-directories-first --time-style=long-iso")
+  (dired-listing-switches "-lagGhS --group-directories-first --time-style=long-iso")
   (dired-dwim-target t)
   (dired-recursive-deletes 'always)
   (dired-recursive-copies 'always)
@@ -25,6 +25,12 @@
 
 (use-package dired-x :requires dired)
 (use-package dired-avfs :requires dired)
+
+(use-package all-the-icons-dired
+  :diminish
+  all-the-icons-dired-mode
+  :hook
+  (dired-mode . all-the-icons-dired-mode))
 
 (use-package diff-hl
   :requires dired
