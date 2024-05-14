6535e563a0c9591085aa9b40672e09407567f8df
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-05-14 15:08:27 +0200

    Paredit on for Clojure code

--- lisp/larsen-paredit.el
+++ lisp/larsen-paredit.el
@@ -1,5 +1,5 @@
 (use-package paredit
-  :hook ((emacs-lisp-mode lisp-mode lisp-interaction-mode scheme-mode) . enable-paredit-mode))
+  :hook ((emacs-lisp-mode lisp-mode lisp-interaction-mode scheme-mode clojure-mode) . enable-paredit-mode))
 
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
