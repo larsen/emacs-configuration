d3a565b79c9d7156272fd32e791611cb27eefbdd
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-05-14 18:20:49 +0200

    better style

--- lisp/larsen-paredit.el
+++ lisp/larsen-paredit.el
@@ -1,5 +1,11 @@
 (use-package paredit
-  :hook ((emacs-lisp-mode lisp-mode lisp-interaction-mode scheme-mode clojure-mode) . enable-paredit-mode))
+  :hook
+  (emacs-lisp-mode . enable-paredit-mode)
+  (lisp-mode . enable-paredit-mode)
+  (lisp-interaction-mode . enable-paredit-mode)
+  (slime-repl-mode . enable-paredit-mode)
+  (scheme-mode . enable-paredit-mode)
+  (clojure-mode . enable-paredit-mode))
 
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
