e0e9f376637c21b2af436b1cd2782df22edbea20
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-07-21 16:06:13 +0200

    + magit-todos

--- lisp/larsen-magit.el
+++ lisp/larsen-magit.el
@@ -20,5 +20,19 @@
   (magit-save-repository-buffers 'dontask))
 
 
+(use-package magit-todos
+  :demand t
+  :after magit
+  :custom
+  (hl-todo-keyword-faces '(("KLUDGE" . "#d0bf8f")
+                           ("HACK" . "#d0bf8f")
+                           ("TODO" . "#D98C32")
+                           ("FIXME" . "#EF681F")
+                           ("WTF" . "#F8240E")))
+  (magit-todos-keywords-list (mapcar #'car hl-todo-keyword-faces))
+  (magit-todos-auto-group-items 50)
+  (magit-todos-exclude-globs '(".git/" "docs/"))
+  :config
+  (magit-todos-mode t))
 
 (provide 'larsen-magit)
