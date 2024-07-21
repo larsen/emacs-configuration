f3d522c9a6862aca2c61ed2686d1bf7338eac004
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-07-21 16:10:44 +0200

    removed stale code

--- lisp/larsen-magit.el
+++ lisp/larsen-magit.el
@@ -1,24 +1,7 @@
 (use-package magit
   :bind (("C-x g" . magit-status))
-  :config
-  ;; from http://whattheemacsd.com/setup-magit.el-01.html
-  ;; full screen magit-status
-
-  (defadvice magit-status (around magit-fullscreen activate)
-    (window-configuration-to-register :magit-fullscreen)
-    ad-do-it
-    (delete-other-windows))
-
-  (defun magit-quit-session ()
-    "Restores the previous window configuration and kills the magit buffer"
-    (interactive)
-    (kill-buffer)
-    (jump-to-register :magit-fullscreen))
-
-  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
   :custom
   (magit-save-repository-buffers 'dontask))
-
 
 (use-package magit-todos
   :demand t
