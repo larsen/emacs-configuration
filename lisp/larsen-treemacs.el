3f2bc411c5638647810ceb88bc270b451e0de966
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-07-21 16:09:54 +0200

    removed stale code

--- lisp/larsen-treemacs.el
+++ lisp/larsen-treemacs.el
@@ -4,17 +4,11 @@
   :bind (:map global-map
               ("C-x t t"   . treemacs-select-window)
               ("C-x t d"   . treemacs-select-directory))
-  :config
-  (treemacs-load-theme "Default")
-  ;;   (treemacs-file-face                :inherit default)
   :custom-face
-  (treemacs-root-face ((t (:inherit 'default :height 0.8))))
-  (treemacs-file-face ((t (:inherit 'default :height 0.8))))
-  (treemacs-root-face ((t (:inherit 'default :height 0.8))))
   (treemacs-window-background-face ((t (:inherit 'mode-line-inactive))))
-  (treemacs-git-*-face ((t (:inherit 'default :height 0.8))))
   :custom
   (treemacs-is-never-other-window t)
+  (treemacs-space-between-root-nodes nil)
   (treemacs-width 25))
 
 (use-package treemacs-projectile
@@ -24,5 +18,8 @@
 (use-package treemacs-magit
   :after (treemacs magit)
   :ensure t)
+
+(use-package lsp-treemacs
+  :after treemacs)
 
 (provide 'larsen-treemacs)
