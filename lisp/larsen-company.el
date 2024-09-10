69b985d724252410a24a8b9fc7bc7a8bd07ca03d
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-09-10 14:44:48 +0200

    keybinding for company-yasnippet

--- lisp/larsen-company.el
+++ lisp/larsen-company.el
@@ -5,6 +5,7 @@
   (company-selection-wrap-around t)
   (company-idle-delay (lambda () (if (company-in-string-or-comment) nil 0.3)))
   ; (company-global-modes '(not erc-mode message-mode eshell-mode))
-  )
+  :bind (:map global-map
+              ("C-c y" . company-yasnippet)))
 
 (provide 'larsen-company)
