f4a9d81960d294f7606346526d10e01b83af3854
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-10-12 21:20:53 +0200

    pyvenv configuration

--- lisp/larsen-python.el
+++ lisp/larsen-python.el
@@ -2,4 +2,17 @@
 
 (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
 
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
+
 (provide 'larsen-python)
