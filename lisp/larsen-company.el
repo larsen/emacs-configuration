13ac23133fbb71b9c99fb7ead6123d9a380b47af
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2024-10-27 07:26:22 +0100

    finetuning company

--- lisp/larsen-company.el
+++ lisp/larsen-company.el
@@ -3,7 +3,8 @@
   (global-company-mode)
   :custom
   (company-selection-wrap-around t)
-  (company-idle-delay (lambda () (if (company-in-string-or-comment) nil 0.3)))
+  (company-idle-delay (lambda () (if (company-in-string-or-comment) nil 0.2)))
+  (company-minimum-prefix-length 1)
   ; (company-global-modes '(not erc-mode message-mode eshell-mode))
   :bind (:map global-map
               ("C-c y" . company-yasnippet)
