faae4e82bf245e26a0828ddba91deb88defc0ad1
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2025-02-21 13:24:31 +0100

    refactoring, removed stale code

--- lisp/larsen-elfeed.el
+++ lisp/larsen-elfeed.el
@@ -1,5 +1,41 @@
 (use-package elfeed
   :ensure t
+  :config
+  (setq-default elfeed-search-filter "@1-week-ago +unread -clutter")
+  (load "elfeed-subscriptions")
+  (defun elfeed-link-title (entry)
+    "Copy the entry title and URL as org link to the clipboard."
+    (interactive)
+    (let* ((link (elfeed-entry-link entry))
+           (title (elfeed-entry-title entry))
+           (titlelink (concat "[[" link "][" title "]]")))
+      (when titlelink
+        (kill-new titlelink)
+        (x-set-selection 'PRIMARY titlelink)
+        (message "Yanked: %s" titlelink))))
+
+  (defun elfeed-db-get-all-tags ()
+    "Return a list of all tags currently in the database."
+    (let ((table (make-hash-table :test 'eq)))
+      (with-elfeed-db-visit (e _)
+        (dolist (tag (elfeed-entry-tags e))
+          (setf (gethash tag table) tag)))
+      (let ((tags ()))
+        (maphash (lambda (k _) (push k tags)) table)
+        (cl-sort tags #'string< :key #'symbol-name))))
+
+  (defun elfeed-helm-tags ()
+    (interactive)
+    (helm :sources '(((name . "elfeed tags")
+                      (volatile)
+                      (elfeed-db-get-all-tags)))))
+  :custom
+  (url-queue-timeout 30)
+  (elfeed-use-curl t)
+  (shr-width 80)
+  :custom-face
+  (interesting-elfeed-entry ((t :foreground "#f77")) "interesting elfeed entry")
+  (personal-elfeed-entry ((t :foreground "#3C913C")) "personal elfeed entry")
   :bind (:map elfeed-show-mode-map
               ("l" . (lambda ()
                        (interactive)
@@ -19,83 +55,6 @@
                            (if use-generic-p
                                (browse-url-generic link)
                              (browse-url link))))))))
-
-(setq-default elfeed-search-filter "@1-week-ago +unread -clutter")
-(setq url-queue-timeout 30)
-(setq elfeed-use-curl t)
-
-;; (setq browse-url-browser-function 'eww-browse-url)
-
-(load "elfeed-subscriptions")
-
-(setq shr-width 80)
-
-(defface interesting-elfeed-entry
-  '((t :foreground "#f77"))
-  "interesting elfeed entry")
-
-(push '(interesting interesting-elfeed-entry)
-      elfeed-search-face-alist)
-
-(defface personal-elfeed-entry
-  '((t :foreground "#3C913C"))
-  "personal elfeed entry")
-
-(push '(personal personal-elfeed-entry)
-      elfeed-search-face-alist)
-
-;; From http://heikkil.github.io/blog/2015/05/09/notes-from-elfeed-entries/
-
-(defun elfeed-link-title (entry)
-  "Copy the entry title and URL as org link to the clipboard."
-  (interactive)
-  (let* ((link (elfeed-entry-link entry))
-         (title (elfeed-entry-title entry))
-         (titlelink (concat "[[" link "][" title "]]")))
-    (when titlelink
-      (kill-new titlelink)
-      (x-set-selection 'PRIMARY titlelink)
-      (message "Yanked: %s" titlelink))))
-
-
-;; (defun my-elfeed-show-entry (buff)
-;;   (popwin:popup-buffer buff
-;;                        :position 'right
-;;                        :width 0.5
-;;                        :dedicated t
-;;                        :stick t))
-
-;; (defun my-elfeed-kill-buffer ()
-;;   (interactive)
-;;   (let ((window (get-buffer-window (get-buffer "*elfeed-entry*"))))
-;;     (kill-buffer (get-buffer "*elfeed-entry*"))
-;;     (delete-window window)))
-
-;; (setq-default elfeed-show-entry-switch #'my-elfeed-show-entry
-;;               elfeed-show-entry-delete #'my-elfeed-kill-buffer)
-
-;; Found in the upstream repo
-;; https://github.com/skeeto/elfeed/commit/d773794ae8a24048aac4a5e3146a884b4b4cdd63
-;; TODO Test & Fix
-
-(defun elfeed-db-get-all-tags ()
-  "Return a list of all tags currently in the database."
-  (let ((table (make-hash-table :test 'eq)))
-    (with-elfeed-db-visit (e _)
-      (dolist (tag (elfeed-entry-tags e))
-        (setf (gethash tag table) tag)))
-    (let ((tags ()))
-      (maphash (lambda (k _) (push k tags)) table)
-      (cl-sort tags #'string< :key #'symbol-name))))
-
-(defun elfeed-helm-tags ()
-  (interactive)
-  (helm :sources '(((name . "elfeed tags")
-                    (volatile)
-                    (elfeed-db-get-all-tags)))))
-
-;; (use-package 'elfeed-goodies)
-;; (elfeed-goodies/setup)
 
 (use-package elfeed-webkit
   :ensure
