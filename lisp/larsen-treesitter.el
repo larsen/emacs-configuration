f1b040215a191b29be5396642a733764f4d5f124
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2026-02-23 17:32:00 +0100

    Disabled treesit-auto, fixes #20

--- lisp/larsen-treesitter.el
+++ lisp/larsen-treesitter.el
@@ -1,8 +1,8 @@
-(use-package treesit-auto
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
+;; (use-package treesit-auto
+;;   :custom
+;;   (treesit-auto-install 'prompt)
+;;   :config
+;;   (treesit-auto-add-to-auto-mode-alist 'all)
+;;   (global-treesit-auto-mode))
 
 (provide 'larsen-treesitter)
