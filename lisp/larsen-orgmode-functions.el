75d0377ef29c2a78f1de162847519a446b0c3760
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2026-07-11 14:40:49 +0200

    bulk rename

--- lisp/larsen-orgmode-functions.el
+++ lisp/larsen-orgmode-functions.el
@@ -1,4 +1,4 @@
-(defun my-org-confirm-babel-evaluate (lang body)
+(defun my/org-confirm-babel-evaluate (lang body)
   (not (string= lang "emacs-elisp")))
 
 ;; Special functions to insert week-based object entries
@@ -6,33 +6,33 @@
 (defun current-year ()
   (nth 5 (decode-time (current-time))))
 
-(defun my-calendar-iso-day-to-gregorian (week-number week-day)
+(defun my/calendar-iso-day-to-gregorian (week-number week-day)
   "Return gregorian day corresponding to WEEK-NUMBER and WEEK-DAY.
 WEEK-DAY is expressed as an integer in the range 0..6:
 1 = Monday, 2 = Tuesday, ..., 0 = Sunday."
   (calendar-gregorian-from-absolute
    (calendar-iso-to-absolute (list week-number week-day (current-year)))))
 
-(defun my-gregorian-date-as-string (date)
+(defun my/gregorian-date-as-string (date)
   (cl-destructuring-bind (month day year) date
   (format "%4d-%02d-%02d" year month day)))
 
-(defun my-current-week-number ()
+(defun my/current-week-number ()
   "Return ISO8601 week number for today."
   (format-time-string "%V" (current-time)))
 
 (defvar day-properties-string ":PROPERTIES:\n:COLUMNS: %25ITEM %TAGS %PRIORITY %TODO\n:END:")
 
-(defun my-week-heading (week-number)
+(defun my/week-heading (week-number)
   "Return a string representing the heading for WEEK-NUMBER todo entries subtree."
-  (let ((week-begin-date (my-calendar-iso-day-to-gregorian week-number 1))
-        (week-end-date (my-calendar-iso-day-to-gregorian week-number 5)))
+  (let ((week-begin-date (my/calendar-iso-day-to-gregorian week-number 1))
+        (week-end-date (my/calendar-iso-day-to-gregorian week-number 5)))
     (format "Week %d (%s - %s)"
             week-number
-            (my-gregorian-date-as-string week-begin-date)
-            (my-gregorian-date-as-string week-end-date))))
+            (my/gregorian-date-as-string week-begin-date)
+            (my/gregorian-date-as-string week-end-date))))
 
-(defun my-today-heading ()
+(defun my/today-heading ()
   "Return a string representing the heading for today's todo entries subtree."
   (format-time-string "%A" (current-time)))
 
@@ -56,10 +56,10 @@ WEEK-DAY is expressed as an integer in the range 0..6:
         (dot . t)
         (latex . t)))
 
-(defun my-insert-current-week-item (week-number)
+(defun my/insert-current-week-item (week-number)
   "Insert 1-week-worth structure for entering todo entries for WEEK-NUMBER."
   (interactive "P")
-  (insert (format "* %s\n" (my-week-heading week-number)))
+  (insert (format "* %s\n" (my/week-heading week-number)))
   (dolist (week-day-name '(Monday
                            Tuesday
                            Wednesday
@@ -69,13 +69,13 @@ WEEK-DAY is expressed as an integer in the range 0..6:
                            Sunday))
      (insert (format "** %s\n%s\n" week-day-name day-properties-string))))
 
-(defun my-org-set-item-deadline (week-number)
-  (let ((week-end-date (my-calendar-iso-day-to-gregorian week-number 5)))
-    (org-deadline nil (my-gregorian-date-as-string week-end-date))))
+(defun my/org-set-item-deadline (week-number)
+  (let ((week-end-date (my/calendar-iso-day-to-gregorian week-number 5)))
+    (org-deadline nil (my/gregorian-date-as-string week-end-date))))
 
-(defun my-org-set-tree-deadline (week-number)
+(defun my/org-set-tree-deadline (week-number)
   (interactive "P")
-  (org-map-entries (lambda () (my-org-set-item-deadline week-number)) nil 'tree))
+  (org-map-entries (lambda () (my/org-set-item-deadline week-number)) nil 'tree))
 
 ;; Experimental
 ;; a more natural way to use indirect buffers
@@ -87,10 +87,10 @@ WEEK-DAY is expressed as an integer in the range 0..6:
   (org-tree-to-indirect-buffer)
   (windmove-right))
 
-(defun my-find-today-heading ()
-  (let ((m (org-find-olp `(,(my-week-heading
-                             (string-to-number (my-current-week-number)))
-                           ,(my-today-heading))
+(defun my/find-today-heading ()
+  (let ((m (org-find-olp `(,(my/week-heading
+                             (string-to-number (my/current-week-number)))
+                           ,(my/today-heading))
                          ;; In current buffer, because we're using file+function
                          t)))
     (set-buffer (marker-buffer m))
@@ -99,10 +99,10 @@ WEEK-DAY is expressed as an integer in the range 0..6:
     (goto-char m)
     (set-marker m nil)))
 
-(defun my-org-jump-to-today ()
+(defun my/org-jump-to-today ()
   "Move cursor to the heading corresponding to today."
   (interactive)
-  (my-find-today-heading))
+  (my/find-today-heading))
 
 (defun orgzly-files ()
   (file-expand-wildcards "~/Dropbox/orgzly/*.org"))
@@ -130,7 +130,7 @@ WEEK-DAY is expressed as an integer in the range 0..6:
 ;; In flameshot, set filename as "screenshot", and the command as "flameshot gui -p /tmp", so that we always ends up
 ;; with /tmp/screenshot.png. Nullify org-download-screenshot-method by setting it to `echo', so that essentially we
 ;; are only calling (org-download-image org-download-screenshot-file).
-(defun my-org-download-screenshot ()
+(defun my/org-download-screenshot ()
   "Capture screenshot and insert the resulting file.
 The screenshot tool is determined by `org-download-screenshot-method'."
   (interactive)
@@ -141,6 +141,6 @@ The screenshot tool is determined by `org-download-screenshot-method'."
     (while (not (file-exists-p tmp-file))
       (sleep-for 2))
     (org-download-image tmp-file)))
-(global-set-key (kbd "<C-print>") 'my-org-download-screenshot)
+(global-set-key (kbd "<C-print>") 'my/org-download-screenshot)
 
 (provide 'larsen-orgmode-functions)
